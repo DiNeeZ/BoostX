5b12d5044df02160faa617b2bb8a400bc6af7938
DiNeeZ <dneez86@gmail.com>
2022-05-20 00:18:56 +0300

    added burger menu

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Button from '../button/button'
+import Navbar from '../nav-bar/nav-bar';
 import { ReactComponent as Logo } from '../../assets/images/logo.svg';
 
 import './header.scss'
@@ -12,17 +12,7 @@ const Header = () => {
         <a href='/#' className='header__logo' aria-label='go to main page'>
           <Logo />
         </a>
-        <nav className='header__nav'>
-          <ul className='list-reset header__nav-list nav-list'>
-            <li className='nav-list__item'><a href='/#'>Features</a></li>
-            <li className='nav-list__item'><a href='/#'>FAQs</a></li>
-            <li className='nav-list__item'><a href='/#'>About</a></li>
-          </ul>
-        </nav>
-        <div className='header__right'>
-          <a href='/#' className='header__sign-in'>Sign In</a>
-          <Button shadow>Start Free</Button>
-        </div>
+        <Navbar />
       </div>
     </header>
   )
---
+++ src/components/nav-bar/nav-bar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import Button from '../button/button'
+import './nav-bar.scss'
+
+const Navbar = () => {
+  const [ isOpen, setIsOpen ] = useState(false)
+
+  return (
+    <>
+      <nav className={`nav ${isOpen && 'open'}`}>
+        <ul className='list-reset nav__list'>
+          <li className='nav__list-item'><a href='/#'>Features</a></li>
+          <li className='nav__list-item'><a href='/#'>FAQs</a></li>
+          <li className='nav__list-item'><a href='/#'>About</a></li>
+        </ul>
+        <div className='nav__right'>
+          <a href='/#' className='nav__sign-in'>Sign In</a>
+          <Button shadow>Start Free</Button>
+        </div>
+      </nav>
+      <div className={`burger-btn ${isOpen && 'open'}`} onClick={() => setIsOpen(!isOpen)}>
+        <div />
+        <div />
+        <div />
+      </div>
+    </>
+  )
+}
+
+export default Navbar
